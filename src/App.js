9f1fb2b1e02d5fdcdc5bff6862027aa45e6be8c8
AustineGwa <austinegwa64@gmail.com>
2021-10-14 17:01:50 +0300

    implement playing and  pausing music

--- src/App.js
+++ src/App.js
@@ -12,10 +12,14 @@ function App() {
 
   const [songs , setSongs] = useState(data())
   const [currentSong, setCurrentSong] = useState(songs[1])
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="App">
       <Song currentSong = {currentSong}/>
-      <Player currentSong={currentSong}/>
+      <Player
+       setIsPlaying={setIsPlaying}
+       isPlaying={isPlaying}
+       currentSong={currentSong}/>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,11 +2,19 @@ import { faAngleLeft, faAngleRight, faPlay } from "@fortawesome/free-solid-svg-i
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, {useRef} from "react";
 
-const Player = ({currentSong}) =>{
+const Player = ({currentSong, isPlaying, setIsPlaying}) =>{
 
     const audioRef = useRef(null)
     const playSongHandler = () =>{
-      audioRef.current.play()
+
+        if(isPlaying){
+            audioRef.current.pause()
+            setIsPlaying(!isPlaying)
+        }else{
+            audioRef.current.play()
+            setIsPlaying(!isPlaying)
+        }
+     
     }
     return(
         <div className="player">
