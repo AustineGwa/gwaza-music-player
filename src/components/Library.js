0885b75408e1c422bdfcca4d7d167a1b922d1836
AustineGwa <austinegwa64@gmail.com>
2021-10-26 12:15:11 +0300

    nightly builds

---
+++ src/components/Library.js
@@ -0,0 +1,16 @@
+import react from "react";
+import LibrarySong from "./LibrarySong";
+
+const library = ({songs}) => {
+
+    return (
+        <div className="library">
+            <h2>Library</h2>
+            <div className="library-songs">
+                {songs.map(song =><LibrarySong song = {song} /> )}
+            </div>
+        </div>
+    )
+}
+
+export default library
